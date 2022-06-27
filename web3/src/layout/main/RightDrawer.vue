90bf08ca5e6d58b22b09eb1af9165aad97705ce8
naqunmiemie <944081949@qq.com>
2022-06-27 18:36:18 +0800

    commit

--- web3/src/layout/main/RightDrawer.vue
+++ web3/src/layout/main/RightDrawer.vue
@@ -1,9 +1,11 @@
 <template>
   <el-drawer v-model="store.drawer" title="My wallet" direction="rtl" @open="openDrawer()">
-    <el-card class="connectMetamask" shadow="hover" @click="connectMetamask()">
-      <span>MetaMask</span>
-      <span class="net">please choose rinkeby</span>
-    </el-card>
+    <el-tooltip content="connect" placement="top">
+      <el-card class="connectMetamask" shadow="hover" @click="connectMetamask()">
+        <span>MetaMask</span>
+        <span class="net">please choose rinkeby</span>
+      </el-card>
+    </el-tooltip>
 
     <el-tooltip content="copy" placement="top">
       <el-card class="copyAccount" shadow="hover" @click="copyAccount()">
@@ -16,20 +18,21 @@
 
 
     <div class="balance">
-      <el-card shadow="never"> <span>LuuToken</span> <span class="sum">{{ store.LuuTokenBalance }}</span></el-card>
-      <el-card shadow="never"> <span>ETH</span> <span class="sum">0</span></el-card>
-      <el-card shadow="never"> <span>USDT</span> <span class="sum">0</span></el-card>
+      <el-card shadow="never"> <span>LuuToken</span> <span class="sum">{{ store.luuTokenBalance }}</span></el-card>
+      <el-card shadow="never"> <span>ETH</span> <span class="sum">{{ store.ethBalance }}</span></el-card>
+      <el-card shadow="never"> <span>USDT</span> <span class="sum">{{ store.usdtBalance }}</span></el-card>
     </div>
   </el-drawer>
 
 </template>
 
 <script lang="ts" setup>
-import { LuuTokenAddress, provider, signer, useStore } from '../../store/index';
+import { LuuTokenAddress, useStore } from '../../store/index';
 import { BigNumber, ethers } from "ethers";
 import { ElMessage } from 'element-plus';
 import { LuuToken__factory } from '../../../../contract/src/types/factories/contracts/LuuToken__factory';
 import { LuuToken } from '../../../../contract/src/types/contracts/LuuToken';
+import { markRaw } from 'vue';
 
 const store = useStore()
 
@@ -42,14 +45,14 @@ async function connectMetamask(): Promise<boolean> {
     return false;
   }
 
-  provider = new ethers.providers.Web3Provider(window.ethereum)
-  console.log(await provider.getBlockNumber());
+  store.provider = markRaw(new ethers.providers.Web3Provider(window.ethereum))
 
   const accounts = <string[]>await store.provider.send("eth_requestAccounts", []);
-  if (provider && accounts.length !== 0) {
+  if (store.provider && accounts.length !== 0) {
     store.account = accounts[0];
-    signer = provider.getSigner()
+    store.signer = store.provider.getSigner()
     console.log(`account:${accounts[0]}`);
+    getBalance();
     return true;
   } else {
     console.log(`no authorized accoutn found`);
@@ -62,16 +65,24 @@ async function copyAccount() {
   await navigator.clipboard.writeText(store.account)
 }
 
-async function getBalanceOfLtk() {
-  if (provider && signer && store.account !== "") {
-    const luuTokenContract = new ethers.Contract(LuuTokenAddress, LuuToken__factory.abi, store.signer) as LuuToken;
-    console.log(store.account);
-    // await store.provider.getBlockNumber()
-    // console.log(await store.provider.getBlockNumber());
-    // await luuTokenContract.balanceOf("0xda4d32877a70f3a4490008df66f2dd988451a431")
+function getBalance() {
+  getBalanceOfEth()
+  getBalanceOfLtk()
+}
 
-    // const abc = (await luuTokenContract.balanceOf(store.account)).toNumber
-    // console.log("type" + typeof (abc));
+async function getBalanceOfLtk() {
+  if (store.provider && store.signer && store.account !== "") {
+    const luuTokenContract = new ethers.Contract(LuuTokenAddress, LuuToken__factory.abi, store.signer) as LuuToken;
+    await luuTokenContract.balanceOf("0xda4d32877a70f3a4490008df66f2dd988451a431")
+    store.luuTokenBalance = (await luuTokenContract.balanceOf(store.account)).toString()
+  } else {
+    console.log('Please connect MetaMask!');
+  }
+}
+
+async function getBalanceOfEth() {
+  if (store.provider && store.signer && store.account !== "") {
+    store.ethBalance = (await store.provider.getBalance(store.account)).toString();
   } else {
     console.log('Please connect MetaMask!');
   }
@@ -79,7 +90,7 @@ async function getBalanceOfLtk() {
 
 function openDrawer() {
   console.log("openDrawer");
-  getBalanceOfLtk()
+  getBalance()
 
 }
 
--- web3/src/store/index.ts
+++ web3/src/store/index.ts
@@ -6,14 +6,14 @@ export const useStore = defineStore(APP_STORE, {
   state: () => {
     return {
       drawer: false,
-      account: "" as string,
-      LuuTokenBalance: 0,
-      ethBalance: 0,
-      usdtBalance: 0,
+      account: "",
+      luuTokenBalance: "",
+      ethBalance: "",
+      usdtBalance: "",
+      provider: null as Web3Provider | null,
+      signer:null as JsonRpcSigner | null,
     };
   },
 });
 
 export const LuuTokenAddress = "0x756C577C1489EE3390FF010028FFd9c57329ce20";
-export const provider: Web3Provider | null = null;
-export const signer: JsonRpcSigner | null = null;
