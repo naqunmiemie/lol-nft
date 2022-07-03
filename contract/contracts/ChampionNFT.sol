58360ea278f6b6375d57899e0f22e60e6b5f9618
naqunmiemie <45996643+naqunmiemie@users.noreply.github.com>
2022-07-03 23:55:30 +0800

    commit

--- contract/contracts/ChampionNFT.sol
+++ contract/contracts/ChampionNFT.sol
@@ -1,19 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract ChampionNFT is ERC721, ERC721URIStorage, Pausable, Ownable {
-    using Counters for Counters.Counter;
+contract ChampionNFT is
+    Initializable,
+    ERC721Upgradeable,
+    ERC721URIStorageUpgradeable,
+    PausableUpgradeable,
+    OwnableUpgradeable,
+    UUPSUpgradeable
+{
+    using CountersUpgradeable for CountersUpgradeable.Counter;
 
-    Counters.Counter private _tokenIdCounter;
+    CountersUpgradeable.Counter private _tokenIdCounter;
 
-    constructor() ERC721("ChampionNFT", "ChampionNFT") {}
+    function initialize() public initializer {
+        __ERC721_init("ChampionNFT", "ChampionNFT");
+        __ERC721URIStorage_init();
+        __Pausable_init();
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
 
+    //onlyOwner
+    //onlyOwner
+    //onlyOwner
     function pause() public onlyOwner {
         _pause();
     }
@@ -29,26 +47,46 @@ contract ChampionNFT is ERC721, ERC721URIStorage, Pausable, Ownable {
         _setTokenURI(tokenId, uri);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        whenNotPaused
-        override
-    {
+    //internal
+    //internal
+    //internal
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override whenNotPaused {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
     // The following functions are overrides required by Solidity.
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
         super._burn(tokenId);
     }
 
+    //public
+    //public
+    //public
     function tokenURI(uint256 tokenId)
         public
         view
-        override(ERC721, ERC721URIStorage)
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    function ownedTokensId(address addr) public view returns (uint256[] memory) {
+        uint256 maxTokenId = _tokenIdCounter.current();
+        require(maxTokenId > 0, "NFT has not been minted yet");
+        uint256[] memory tokenIds;
+        for (uint256 tokenId = 1; tokenId < maxTokenId; ++tokenId) {
+            if (ownerOf(tokenId) == addr) {
+                tokenIds.push(tokenId);
+            }
+        }
+        return tokenIds;
     }
 }
