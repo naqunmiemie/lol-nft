c7005ecd058ee42bffeec65d7a9ea0ac035fba7a
naqunmiemie <45996643+naqunmiemie@users.noreply.github.com>
2022-07-08 00:04:51 +0800

    commit

--- web3/src/layout/main/championInformation.ts
+++ web3/src/layout/main/championInformation.ts
@@ -3,21 +3,24 @@ import { useStore } from "../../store";
 import { ChampionNFTAddress } from "../../utils/conctract/SomeAddress";
 import { ChampionNFT } from "../../../../contract/src/types/contracts/ChampionNFT";
 import { ChampionNFT__factory } from "../../../../contract/src/types/factories/contracts/ChampionNFT__factory";
+import superagent from "superagent";
+import cheerio from "cheerio";
 
-const store = useStore();
+const baseUrl = "https://www.ghostoact.com";
 
 export interface ChampionInformation {
   tokenId: BigNumber;
   num: string;
+  uri: string;
   name: string;
   title: string;
   img: string;
-  uri: string;
 }
 
 export async function getChampionInformation(
   tokenId: BigNumber
 ): Promise<ChampionInformation | null> {
+  const store = useStore();
   if (store.provider && store.signer) {
     const championNFTContract = new ethers.Contract(
       ChampionNFTAddress,
@@ -25,11 +28,39 @@ export async function getChampionInformation(
       store.signer
     ) as ChampionNFT;
     const uri = await championNFTContract.tokenURI(tokenId);
-    
+    let championInformation: ChampionInformation = {
+      tokenId: tokenId,
+      num: tokenId.toString(),
+      uri: uri,
+      name: "",
+      title: "",
+      img: "",
+    };
     console.log("prizePool");
-    return null;
+    return reptile(championInformation);
   } else {
     console.log("Please connect MetaMask!");
     return null;
   }
+}
+
+async function reptile(
+  championInformation: ChampionInformation
+): Promise<ChampionInformation> {
+  const result = await superagent.get(championInformation.uri);
+  const text = result.text;
+  const root = cheerio.load(text);
+  const name = root(".mod-name").find("a").html();
+  const title = root(".mod-title").find("a").html();
+  const img = root(".sd-btn.splash-btn.menu-box").find("a").attr("href");
+  if (typeof title === "string") {
+    championInformation.title = title;
+  }
+  if (typeof name === "string") {
+    championInformation.name = name;
+  }
+  if (typeof img === "string") {
+    championInformation.img = baseUrl + img;
+  }
+  return championInformation;
 }
--- web3/src/layout/main/luckyDraw/LuckyDraw.vue
+++ web3/src/layout/main/luckyDraw/LuckyDraw.vue
@@ -1,9 +1,8 @@
 <template>
   <el-row>
-    <el-col v-for="(o) in 2" :key="o" :span="3">
+    <el-col v-for="(info) in championInformations" :key="info.num" :span="3">
       <el-card :body-style="{ padding: '4px' }" class="card" shadow="always">
-        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-          class="image" />
+        <img :src=info.img class="image" />
         <div style="padding: 10px">
           <span>Yummy hamburger</span>
           <div class="bottom">
@@ -17,22 +16,32 @@
 </template>
 
 <script lang="ts" setup>
-import { ethers, BigNumber } from 'ethers';
-import { ref } from 'vue'
+import { BigNumber, ethers } from 'ethers';
+import { ref } from 'vue';
 import { ChampionNFT } from '../../../../../contract/src/types/contracts/ChampionNFT';
 import { ChampionNFT__factory } from '../../../../../contract/src/types/factories/contracts/ChampionNFT__factory';
 import { useStore } from '../../../store';
 import { ChampionNFTAddress } from '../../../utils/conctract/SomeAddress';
+import { getChampionInformation, ChampionInformation } from '../championInformation';
 
 const store = useStore()
 const currentDate = ref(new Date())
+const championInformations = ref(new Array<ChampionInformation>())
 prizePool()
 async function prizePool() {
   if (store.provider && store.signer) {
     const championNFTContract = new ethers.Contract(ChampionNFTAddress, ChampionNFT__factory.abi, store.signer) as ChampionNFT;
     const tokenIds: BigNumber[] = await championNFTContract.getOwnerTokenIds(ChampionNFTAddress);
     for (const tokenId of tokenIds) {
-      
+      console.log(tokenId.toString());
+      const championInformation = getChampionInformation(tokenId);
+      championInformation.then(championInformation => {
+        console.log(championInformation?.uri);
+        if (championInformation != null) {
+          championInformations.value.push(championInformation)
+        }
+      })
+
     }
     console.log('prizePool');
   } else {
